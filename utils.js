0d2b55e95f2c42ff144a9c0f0f69703c118317de
Elendrin <131126848+relorius@users.noreply.github.com>
2026-03-05 20:26:05 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:26:00.861Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T17:26:04.502Z
+Commit: Resolve null pointer exception in user service
