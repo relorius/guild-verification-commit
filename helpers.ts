122b9cdf40a77b7b0976245051c6f53ba2153b6c
Elendrin <131126848+relorius@users.noreply.github.com>
2026-03-05 20:24:35 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:24:30.942Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:24:34.348Z
+Commit: Fix race condition in async operations
