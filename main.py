cf11ea53e1ecb2d403d7427654a827b01626d340
Elendrin <131126848+relorius@users.noreply.github.com>
2026-03-05 20:24:49 +0300

    Introduce real-time notifications

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:24:45.126Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T17:24:48.553Z
+Commit: Introduce real-time notifications
