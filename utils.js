b2ae82cecd935d86793bdc00052010633ddbae7e
Elendrin <131126848+relorius@users.noreply.github.com>
2026-03-05 20:24:32 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:24:28.257Z
-Commit: Add multi-language support
+Timestamp: 2026-03-05T17:24:32.029Z
+Commit: Resolve null pointer exception in user service
