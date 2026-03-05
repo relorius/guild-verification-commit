e0f12b9807c45648fe2cc381ef6a44155f7b4fcf
Elendrin <131126848+relorius@users.noreply.github.com>
2026-03-05 20:25:26 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:25:24.086Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T17:25:25.270Z
+Commit: Fix memory leak in event handlers
