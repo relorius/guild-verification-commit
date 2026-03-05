84040fec8a151166aab3a28553fead97e931fecf
Elendrin <131126848+relorius@users.noreply.github.com>
2026-03-05 20:26:06 +0300

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T17:25:46.680Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T17:26:05.620Z
+Commit: Add multi-language support
